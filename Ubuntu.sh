c0d7b1526842c05e054960836db5477ea917561d
RAGHAVVRAM JOHNSON <raghavvram.johnson@gmail.com>
2025-07-14 21:24:02 +0530

    Create Ubuntu.sh

---
+++ Ubuntu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+# This script creates a rootless Kali Linux distrobox container.
+
+# --- Configuration ---
+CONTAINER_NAME="ubuntu"
+IMAGE="kalilinux/kali-last-release"
+HOME_DIR="$HOME/distrobox/ubuntu-home/"
+
+# --- Script ---
+distrobox create \
+  --image "$IMAGE" \
+  --init \
+  --name "$CONTAINER_NAME" \
+  --home "$HOME_DIR"
+
+echo "Successfully created Ubuntu Linux container named '$CONTAINER_NAME'."
